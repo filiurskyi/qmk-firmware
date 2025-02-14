da26db19e0a3591a007d0cb41e26e055b3945bcc
filiurskyi <anjey.mail@gmail.com>
2025-02-14 16:26:13 +0100

    add caps and change leader key

--- keyboards/nuphy/halo75v2/ansi/keymaps/homerowmods/keymap.c
+++ keyboards/nuphy/halo75v2/ansi/keymaps/homerowmods/keymap.c
@@ -32,12 +32,18 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         case GUI_SCLN:return 225;  // Pinky (slower reaction)
 
 		// Custom timeout for LT(6, KC_ESC) (Caps Lock)
-        case LT(6, KC_ESC): return 300;
+        case LT(4, KC_SPC): return 300;
 
         default:
             return TAPPING_TERM; // Default tapping term for other keys
     }
 }
+
+const uint16_t PROGMEM combo_caps[] = {KC_LSFT, KC_RSFT, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(combo_caps, KC_CAPS), // toggle caps with lshift+rshift
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // layer Mac
@@ -68,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	WIN_PRTA,   KC_INS,     KC_DEL,
 	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,                KC_HOME,
 	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC,	KC_BSLS,                KC_END,
-	LT(4, KC_ESC),	GUI_A,   	ALT_S,   	CTL_D,  	SFT_F,   	KC_G,   	KC_H,   	SFT_J,   	CTL_K,   	ALT_L,  	GUI_SCLN,	KC_QUOT, 	KC_ENT, 	                        KC_PGUP,
+	KC_ESC, 	GUI_A,   	ALT_S,   	CTL_D,  	SFT_F,   	KC_G,   	KC_H,   	SFT_J,   	CTL_K,   	ALT_L,  	GUI_SCLN,	KC_QUOT, 	KC_ENT, 	                        KC_PGUP,
 	KC_LSFT,				KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,                KC_UP,	    KC_PGDN,
-	KC_LCTL,	KC_LWIN,	KC_LALT,										KC_SPC, 							            KC_RALT,	MO(5),	                KC_LEFT,   	KC_DOWN,	KC_RIGHT),
+	KC_LCTL,	KC_LWIN,	KC_LALT,										LT(4, KC_SPC), 							            KC_RALT,	MO(5),	                KC_LEFT,   	KC_DOWN,	KC_RIGHT),
 // layer 6 VIM motions
 [4] = LAYOUT(
 	_______, 	_______,  	_______,  	_______, 	_______,  	_______,  	_______,  	_______,  	_______,  	_______, 	_______, 	_______, 	_______, 	_______,	_______,    _______,
