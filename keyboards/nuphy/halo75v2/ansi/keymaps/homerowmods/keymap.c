792371a0fe432331a6faa54a60bcce7c118ee9d5
filiurskyi <anjey.mail@gmail.com>
2025-02-14 01:10:56 +0100

    typo

--- keyboards/nuphy/halo75v2/ansi/keymaps/homerowmods/keymap.c
+++ keyboards/nuphy/halo75v2/ansi/keymaps/homerowmods/keymap.c
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [5] = LAYOUT(
 	_______, 	KC_BRID,  	KC_BRIU,  	SOCDON, 	    SOCDOFF,  	SOCDTOG,  	_______,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	KC_PSCR,	TOG_POWER_ON_ANIMATION,	_______,
 	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 	_______,	            _______,
-	_______, 	DF(2),   	DF(5),   	_______,   	_______,   	_______,   	DEBOUNCE_PRESS_DEC,   	DEBOUNCE_PRESS_SHOW,   	DEBOUNCE_PRESS_INC,   	TOG_DEEP_SLEEP,  	TOG_USB_SLP,   	DEV_RESET,	SLEEP_MODE, BAT_SHOW,	            _______,
+	_______, 	DF(2),   	DF(3),   	_______,   	_______,   	_______,   	DEBOUNCE_PRESS_DEC,   	DEBOUNCE_PRESS_SHOW,   	DEBOUNCE_PRESS_INC,   	TOG_DEEP_SLEEP,  	TOG_USB_SLP,   	DEV_RESET,	SLEEP_MODE, BAT_SHOW,	            _______,
 	TOG_CAPS_IND,	SLEEP_TIMEOUT_DEC,   	SLEEP_TIMEOUT_SHOW,   	SLEEP_TIMEOUT_INC,  	_______,   	_______,   	DEBOUNCE_RELEASE_DEC,	DEBOUNCE_RELEASE_SHOW,   	DEBOUNCE_RELEASE_INC,   	_______,  	_______,	_______, 	_______,                            _______,
 	_______,				_______,   	_______,   	RGB_TEST,  	_______,    _______,   	_______,	MO(6), 		RGB_SPD,	RGB_SPI,	_______,	_______,	            RGB_VAI,    _______,
 	_______,	_______,	_______,										_______, 							            _______,	MO(5),					RGB_MOD,    RGB_VAD,	RGB_HUI),
